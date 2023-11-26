b5189ccdd1ff3b40206da8ee72ad794d5a414072
Aryan <112746954+A-ryan0408@users.noreply.github.com>
2023-11-26 11:19:19 +0530

    Add files via upload

---
+++ QR_Code_Generator.py
@@ -0,0 +1,12 @@
+import qrcode
+import image
+qr = qrcode.QRCode(
+    version=4,
+    box_size=10,
+    border=4,
+)
+qr.add_data('http://www.unipune.ac.in/')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("arininstagram.png")
